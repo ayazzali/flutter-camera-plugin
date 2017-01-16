dda815521884cf9a4ebc0269be1bb5b332d65e0e
rxlabz <dev@rxlabz.com>
2017-01-17 00:15:56 +0100

    image loading with Image.file(f), random scale switch to force image display update and list of all taken pictures in a Row

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,13 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 final Random random = new Random();
-String filepath = "";
-Future<dynamic> handleGetRandom(Map<String, dynamic> message) async {
-  final double min = message['min'].toDouble();
-  final double max = message['max'].toDouble();
-
-  return <String, double>{
-    'value': (random.nextDouble() * (max - min)) + min
-  };
-}
 
 class HelloServices extends StatefulWidget {
   @override
@@ -25,40 +17,50 @@ class HelloServices extends StatefulWidget {
 }
 
 class _HelloServicesState extends State<HelloServices> {
-  double _latitude;
-  double _longitude;
+  List<String> _filepaths = [];
+  String _filepath = "";
+
+  bool _randomScale = false;
 
   @override
   Widget build(BuildContext context) {
-
     return new Material(
-      child: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Image.network("file:/$filepath"),
-            new RaisedButton(
-              child: new Text('Take Photo'),
-              onPressed: _takePicture
-            ),
-            new Text(filepath),
-          ]
-        )
-      )
-    );
+        child: new Center(
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+          new RaisedButton(
+              child: new Text('Take Photo'), onPressed: _takePicture),
+          new Text(_filepath),
+          new Row(children: [
+            new Switch(
+                value: _randomScale,
+                onChanged: (value) {
+                  setState(() => _randomScale = value);
+                }),
+            new Text('Random scale')
+          ], mainAxisAlignment: MainAxisAlignment.center),
+          new Image.file(new File(_filepath),
+              scale: _randomScale ? 1.0 + random.nextDouble() : 1.0),
+          new Row(children: getImgs())
+        ])));
   }
 
-  Future<Null> _takePicture() async {
-    String reply = await PlatformMessages.sendString('takePictureAndSend','');
+  Image getImg(File f) => new Image.file(f);
+  List<Image> getImgs() =>
+      _filepaths.map((path) => getImg(new File(path))).toList();
 
-    setState((){
-      filepath = reply;
-      print("file:/$filepath");
+  Future<Null> _takePicture() async {
+    String reply = await PlatformMessages.sendString('takePictureAndSend', '');
+
+    setState(() {
+      print("Filepath => $reply");
+      _filepath = reply;
+      _filepaths.add(reply);
     });
   }
 }
 
-main() async{
+main() async {
   runApp(new HelloServices());
-
 }
